96a06a4069750a4b495e3224b50bc305dd5b9719
Makoto Ohashi <228x104x@stu.kobe-u.ac.jp>
2022-06-19 02:40:17 +0900

    数あてゲーム完成

--- kazuate/Kazuate.java
+++ kazuate/Kazuate.java
@@ -5,37 +5,61 @@ public class Kazuate {
     public void kazuate(){
 
         int answer=25;
-        //int count=5;
+        int count=5;
 
         
-
-        System.out.println("私が考えている2桁の正の整数をあててください！解答チャンスは５回までです。");
+        System.out.println("私が考えている2桁の正の整数をあててください！解答チャンスは5回までです。");
         
         KeyBoardInput kb=new KeyBoardInput();
-        int num = kb.input();
 
-        if(num==answer){
-            System.out.println("当たり！");
-        }else if(num<answer){
-            System.out.println("残念！");
-            System.out.println("ヒント："+num+"より大きい数");
+        while(count>0){
 
-            if(num+20<=answer){
-                System.out.println("ヒント："+num+"とは20以上差があるよ！");
-            }
-        }else{
-            System.out.println("残念！");
-            System.out.println("ヒント："+num+"より小さい数");
+            System.out.println("チャンスはあと"+count+"回!");
+
+            int num = 0;
+
+
+            while(num < 10 || num > 99){
             
+                System.out.print("●予想を入力してください：");
+                num = kb.input();
+                System.out.println("あなたの予想は"+num+"ですね。");
+                
+                if(num<10 || num>99){
+                    System.out.println("私が考えているのは2桁の正の整数だよ！");
+                    
+                }
 
-            if(num-20>=answer){
-                System.out.println("ヒント："+num+"とは20以上差があるよ！");
             }
-        }
 
-        // System.out.println(num);
+            if(num==answer){
+                System.out.println("当たり！");
+                break;
+            }else if(num<answer){
+                System.out.println("残念！");
+                System.out.println("ヒント："+num+"より大きい数");
+
+                if(num+20<=answer){
+                    System.out.println("ヒント："+num+"とは20以上差があるよ！");
+                }
+            }else{
+                System.out.println("残念！");
+                System.out.println("ヒント："+num+"より小さい数");
+                
+
+                if(num-20>=answer){
+                    System.out.println("ヒント："+num+"とは20以上差があるよ！");
+                }
+            }
+
+            count=count-1;
 
         
+        }
+
+        System.out.println("答えは"+answer+"でした！");
+
+
     }
     
 }
--- kazuate/KeyBoardInput.java
+++ kazuate/KeyBoardInput.java
@@ -10,13 +10,13 @@ import java.io.IOException;
  */
 // ↓KeyBoardInputという名前のクラスを宣言．大文字で始める．単語のつなぎ目は大文字に
 
-public class KeyBoardInput {
 
-	// public KeyBoardInput(){
-// eyBoardInputクラスのmainメソッド．
-	public int input()  {
+
+public class KeyBoardInput{
+
+    public int input() {
 		/*-------- 入力の準備 --------*/
-		// キーボード（標準入力）の読み取り機（リーダー）
+        // キーボード（標準入力）の読み取り機（リーダー）
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String line; // 読み取り用の文字列バッファ
@@ -24,22 +24,26 @@ public class KeyBoardInput {
 		/*-------- 整数の入力の練習 --------*/
 
         int input = 0;
-		try {
-			System.out.print("●予想を入力してください：");
-			line = br.readLine(); // 1行読み込む
-			input = Integer.parseInt(line);// 文字列を整数に変換する．
-			System.out.println("あなたの予想は"+input+"ですね。");
-		} catch (NumberFormatException e) { // parseInt()からの例外を拾う．
-			System.err.println("フォーマット例外：予想をを10で進めます．");
-			input = 10;
-		} catch (IOException e) { // readLine()からの例外を拾う．
-			System.err.println("入出力例外：予想を10で進めます．");
-			input = 10;
+
+		int check=0;
+
+
+		
+		while(check==0){
+			try {
+				line = br.readLine(); // 1行読み込む
+				input = Integer.parseInt(line);// 文字列を整数に変換する．
+
+				check=1;
+			} catch (NumberFormatException e) { // parseInt()からの例外を拾う．
+				System.err.println("フォーマット例外です。もう一度入力してください");
+			} catch (IOException e) { // readLine()からの例外を拾う．
+				System.err.println("入出力例外です。もう一度入力してください。");
+			}
+
 		}
 
-
-
-
+		
         return input;
 	}
 
